006dd35326d039c56ec21662af8f5720e325241c
RIcardo Batel <ricardo.batel@hotmail.com>
2022-11-17 15:58:07 -0300

    utilizando getters e setters para manipular dados

--- oo_pilar_abstracao.php
+++ oo_pilar_abstracao.php
@@ -4,13 +4,38 @@
     class Funcionario {
 
         //atributos
-        public $nome = 'José';
-        public $telefone = '11 9999 9999';
-        public $numFilhos = 2;
+        public $nome = null;
+        public $telefone = null;
+        public $numFilhos = null;
+
+        //getters e setters
+        function setNome($nome) {
+            $this->nome = $nome;
+        }
+
+        function setNumFilhos($numFilhos) {
+            $this->numFilhos = $numFilhos;
+        }
+
+        function setTelefone($telefone){
+            $this->telefone = $telefone;
+        }
+
+        function getNome() {
+            return $this->nome;
+        }
+
+        function getNumFilhos() {
+            return $this->numFilhos;
+        }
+
+        function getTelefone() {
+            return $this->telefone;
+        }
 
         //métodos
         function resumirCadFunc() {
-            return "$this->nome possui $this->numFilhos filho(s)";
+            return "$this->nome possui $this->numFilhos filho(s), o número de telefone é: $this->telefone.";
         }
 
         function modificarNumFilhos($numFilhos) {
@@ -20,16 +45,17 @@
     }
 
     $y = new Funcionario();
-    echo $y->resumirCadFunc();
-    echo '<br/>';
-    $y->modificarNumFilhos(3);
-    echo $y->resumirCadFunc();
+    $y->setNome('José');
+    $y->setNumFilhos(2);
+    $y->setTelefone('11 2222 2222');
+    echo $y->getNome().' possui '.$y->getNumFilhos().' filho(s) e seu telefone é: '.$y->getTelefone();
+
     echo '<hr/>';
 
     $x = new Funcionario();
-    echo $x->resumirCadFunc();
-    echo '<br/>';
-    $x->modificarNumFilhos(1);
-    echo $x->resumirCadFunc();
+    $x->setNome('Ricardo');
+    $x->setNumFilhos(0);
+    $x->setTelefone('48 9999 9999');
+    echo $x->getNome().' possui '.$x->getNumFilhos().' filho(s) e seu telefone é: '.$x->getTelefone();   
 
 ?>
